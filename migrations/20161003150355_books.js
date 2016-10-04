17049b0b252d14c6af40d1cb7e29b514db8c4239
mikeechen <mikeechen915@gmail.com>
2016-10-04 09:09:54 -0700

    part1 done

---
+++ migrations/20161003150355_books.js
@@ -0,0 +1,16 @@
+'use strict';
+exports.up = function(knex) {
+  return knex.schema.createTable('books', (ta) => {
+    ta.increments();
+    ta.string('title').notNullable().defaultTo('');
+    ta.string('author').notNullable().defaultTo('');
+    ta.string('genre').notNullable().defaultTo('');
+    ta.text('description').notNullable().defaultTo('');
+    ta.text('cover_url').notNullable().defaultTo('');
+    ta.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('books');
+};
