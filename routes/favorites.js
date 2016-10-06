1ad77d6a4de576075d12246b1417c9b5e6b078c3
mikeechen <mikeechen915@gmail.com>
2016-10-06 16:30:53 -0700

    before pulling

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const express = require('express');
+const boom = require('boom');
+const knex = require('../knex');
+const jwt = require('jsonwebtoken');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,81 @@
 'use strict';
 
 const express = require('express');
+const boom = require('boom');
+const knex = require('../knex');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt-as-promised');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+const authorize = (req, res, next) => {
+  jwt.verify(req.cookies.accessToken, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      req.verify = false;
+    } else {
+      req.verify = true;
+    }
+
+    next();
+  });
+}
 // YOUR CODE HERE
+router.get('/token', authorize, (req, res, _next) => {
+  res.json(req.verify);
+});
+
+router.post('/token', (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !email.trim()) {
+  return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(400, 'Password must not be blank'));
+  }
+
+  let user;
+
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(400, 'Bad email or password');
+      }
+
+      user = camelizeKeys(row);
+
+      return bcrypt.compare(password, user.hashedPassword);
+    })
+    .then(() => {
+      delete user.hashedPassword;
+
+      const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET, {
+        expiresIn: '3h'
+      });
+
+      res.cookie('accessToken', token, {
+        httpOnly: true,
+        secure: router.get('env') === 'production'
+      });
+
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/token', (req, res, next) => {
+  res.clearCookie('token');
+  res.sendStatus(200);
+});
 
 module.exports = router;
